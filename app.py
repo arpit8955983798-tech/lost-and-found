9197dcdf8905de52a9da0f8ad834a9d6e20095a1
Arpit <arpit8955983798@gmail.com>
2026-09-08 15:39:47 +0530

    Update website

--- app.py
+++ app.py
@@ -451,8 +451,8 @@ def test_ml():
     return f"TF-IDF Similarity: {score}%"
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
